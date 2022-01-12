32732201e59681a9a1f4108d597e1d34d3c50ce6
Francisco Martinez <fran@educareality.com>
2022-01-12 11:56:19 +0100

    funcion boton play/pause

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
 /* Funcion para retornar a la ventana principal de la galería */
 
-returnGaleria = () =>{
-    let displayActivo = document.getElementById("ocultar-galeria");
-    let activarImg360 = document.getElementById("activar-vista-360");
-    activarImg360.style.display = "none";
-    displayActivo.style.display = "";
-}
+returnGaleria = () => {
+  let displayActivo = document.getElementById("ocultar-galeria");
+  let activarImg360 = document.getElementById("activar-vista-360");
+  activarImg360.style.display = "none";
+  displayActivo.style.display = "";
+};
+
+/* Función Play / Pause */
+
+pause = () => {
+  var modelViewer = document.querySelector("#Vr");
+  console.log(modelViewer);
+  var txt = document.querySelector(".pause");
+  if (modelViewer.className === "Model3d activo") {
+    modelViewer.pause();
+    modelViewer.classList.remove("activo");
+    txt.innerHTML = "Play";
+    modelViewer.removeAttribute("autoplay", "");
+  } else {
+    modelViewer.play();
+    modelViewer.classList.add("activo");
+    txt.innerHTML = "Pause";
+    modelViewer.setAttribute("autoplay", "true");
+  }
+};
