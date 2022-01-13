4a2772dc81995ee21c0297494a6dde0be1156300
Francisco Martinez <fran@educareality.com>
2022-01-13 10:38:02 +0100

    Integración de funciones a galeria de imágenes y función play / pause a 3D

--- js/main.js
+++ js/main.js
@@ -11,7 +11,6 @@ returnGaleria = () => {
 
 pause = () => {
   var modelViewer = document.querySelector("#Vr");
-  console.log(modelViewer);
   var txt = document.querySelector(".pause");
   if (modelViewer.className === "Model3d activo") {
     modelViewer.pause();
@@ -25,3 +24,66 @@ pause = () => {
     modelViewer.setAttribute("autoplay", "true");
   }
 };
+
+/* función selector de imagenes */
+
+const items = document.querySelectorAll("img.imgItems");
+const imgActive = document.querySelector('#imgGalery');
+const itemCount = items.length;
+const nextItem = document.querySelector(".next");
+const previousItem = document.querySelector(".previous");
+let count = 0;
+
+function showNextItem() {
+  items[count].classList.remove("active");
+  time = ()=>{
+    imgActive.classList.remove('active');
+  }
+
+  if (count < itemCount -1) {
+    count++;
+  } else {
+    count = 0;
+  }
+
+  let src = items[count];
+  let newSrc = src.getAttribute('src');
+  document.querySelector('#imgGalery').setAttribute('src', newSrc);
+  items[count].classList.add("active");
+  imgActive.classList.add('active');
+  setTimeout(time, 1000);
+}
+
+function showPreviousItem() {
+  items[count].classList.remove("active");
+  time = ()=>{
+    imgActive.classList.remove('active');
+  }
+
+  if (count > 0) {
+    count--;
+  } else {
+    count = itemCount - 1;
+  }
+  
+  let src = items[count];
+  let newSrc = src.getAttribute('src');
+  document.querySelector('#imgGalery').setAttribute('src', newSrc);
+  items[count].classList.add("active");
+  imgActive.classList.add('active');
+  setTimeout(time, 1000);
+}
+
+function keyPress(e) {
+  e = e || window.event;
+
+  if (e.keyCode == "37") {
+    showPreviousItem();
+  } else if (e.keyCode == "39") {
+    showNextItem();
+  }
+}
+
+nextItem.addEventListener("click", showNextItem);
+previousItem.addEventListener("click", showPreviousItem);
+document.addEventListener("keydown", keyPress);
