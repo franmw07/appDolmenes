0ddda478053a268cfa63ad9a0082c675554ba052
Francisco Martinez <fran@educareality.com>
2022-01-13 15:44:24 +0100

    función botones cambio de imagnes (360 y vr) y cambios de información

--- js/main.js
+++ js/main.js
@@ -1,41 +1,39 @@
 /* Funcion para retornar a la ventana principal de la galería */
 
 returnGaleria = () => {
-  let displayActivo = document.getElementById("ocultar-galeria");
-  let activarImg360 = document.getElementById("activar-vista-360");
-  activarImg360.style.display = "none";
-  displayActivo.style.display = "";
+  let displayActivo = document.querySelector('#ocultar-galeria');
+  let activarImg360 = document.querySelector('#activar-vista-360');
+  activarImg360.style.display = 'none';
+  displayActivo.style.display = '';
 };
 
 /* Función Play / Pause */
 
 pause = () => {
-  var modelViewer = document.querySelector("#Vr");
-  var txt = document.querySelector(".pause");
-  if (modelViewer.className === "Model3d activo") {
+  var modelViewer = document.querySelector('#Vr');
+  var txt = document.querySelector('.pause');
+  if (modelViewer.className === 'Model3d activo') {
     modelViewer.pause();
-    modelViewer.classList.remove("activo");
-    txt.innerHTML = "Play";
-    modelViewer.removeAttribute("autoplay", "");
+    modelViewer.classList.remove('activo');
+    txt.innerHTML = 'Play';
+    modelViewer.removeAttribute('autoplay', '');
   } else {
     modelViewer.play();
-    modelViewer.classList.add("activo");
-    txt.innerHTML = "Pause";
-    modelViewer.setAttribute("autoplay", "true");
+    modelViewer.classList.add('activo');
+    txt.innerHTML = 'Pause';
+    modelViewer.setAttribute('autoplay', 'true');
   }
 };
 
 /* función selector de imagenes */
 
-const items = document.querySelectorAll("img.imgItems");
+const items = document.querySelectorAll('img.imgItems');
 const imgActive = document.querySelector('#imgGalery');
 const itemCount = items.length;
-const nextItem = document.querySelector(".next");
-const previousItem = document.querySelector(".previous");
 let count = 0;
 
-function showNextItem() {
-  items[count].classList.remove("active");
+showNextItem =()=> {
+  items[count].classList.remove('active');
   time = ()=>{
     imgActive.classList.remove('active');
   }
@@ -49,13 +47,50 @@ function showNextItem() {
   let src = items[count];
   let newSrc = src.getAttribute('src');
   document.querySelector('#imgGalery').setAttribute('src', newSrc);
-  items[count].classList.add("active");
+  items[count].classList.add('active');
   imgActive.classList.add('active');
   setTimeout(time, 1000);
+
+  switch (newSrc) {
+    case '../../media/02_Jose_Arana.jpeg': 
+      document.querySelector('.container_text_maps').innerHTML = `<li class="fs-4">Nombre Jose Arana 02</li>
+                                                                  <li class="fs-4">Ubicación</li>
+                                                                  <li class="fs-4">Fecha</li>
+                                                                  <li class="fs-4">...</li>`;
+      break;
+    case '../../media/15_Jose_Arana.jpeg':
+      document.querySelector('.container_text_maps').innerHTML = `<li class="fs-4">Nombre Jose Arana 15</li>
+                                                                  <li class="fs-4">Ubicación</li>
+                                                                  <li class="fs-4">Fecha</li>
+                                                                  <li class="fs-4">...</li>`; 
+      break;
+    case '../../media/dolmen-2.jpg':
+      document.querySelector('.container_text_maps').innerHTML = `<li class="fs-4">Nombre Dolmen-2</li>
+                                                                  <li class="fs-4">Ubicación</li>
+                                                                  <li class="fs-4">Fecha</li>
+                                                                  <li class="fs-4">...</li>`; 
+      break;
+    case '../../media/Laskorain_HH3_1Solairua_EsperientziaTxokoa1_11.jpeg':
+      document.querySelector('.container_text_maps').innerHTML = `<li class="fs-4">Nombre Laskorain HH3</li>
+                                                                  <li class="fs-4">Ubicación</li>
+                                                                  <li class="fs-4">Fecha</li>
+                                                                  <li class="fs-4">...</li>`; 
+      break;
+    case '../../media/Laskorain_HH4_1Solairua_Zentzumen_Txokoa1_29.jpeg':
+      document.querySelector('.container_text_maps').innerHTML = `<li class="fs-4">Nombre Laskorain HH4</li>
+                                                                  <li class="fs-4">Ubicación</li>
+                                                                  <li class="fs-4">Fecha</li>
+                                                                  <li class="fs-4">...</li>`; 
+      break;
+
+    default:
+      console.log('error');
+      break;
+  }
 }
 
-function showPreviousItem() {
-  items[count].classList.remove("active");
+showPreviousItem =()=> {
+  items[count].classList.remove('active');
   time = ()=>{
     imgActive.classList.remove('active');
   }
@@ -69,21 +104,167 @@ function showPreviousItem() {
   let src = items[count];
   let newSrc = src.getAttribute('src');
   document.querySelector('#imgGalery').setAttribute('src', newSrc);
-  items[count].classList.add("active");
+  items[count].classList.add('active');
   imgActive.classList.add('active');
   setTimeout(time, 1000);
-}
 
-function keyPress(e) {
-  e = e || window.event;
+  switch (newSrc) {
+    case '../../media/02_Jose_Arana.jpeg': 
+      document.querySelector('.container_text_maps').innerHTML = `<li class="fs-4">Nombre Jose Arana 02</li>
+                                                                  <li class="fs-4">Ubicación</li>
+                                                                  <li class="fs-4">Fecha</li>
+                                                                  <li class="fs-4">...</li>`;
+      break;
+    case '../../media/15_Jose_Arana.jpeg':
+      document.querySelector('.container_text_maps').innerHTML = `<li class="fs-4">Nombre Jose Arana 15</li>
+                                                                  <li class="fs-4">Ubicación</li>
+                                                                  <li class="fs-4">Fecha</li>
+                                                                  <li class="fs-4">...</li>`; 
+      break;
+    case '../../media/dolmen-2.jpg':
+      document.querySelector('.container_text_maps').innerHTML = `<li class="fs-4">Nombre Dolmen-2</li>
+                                                                  <li class="fs-4">Ubicación</li>
+                                                                  <li class="fs-4">Fecha</li>
+                                                                  <li class="fs-4">...</li>`; 
+      break;
+    case '../../media/Laskorain_HH3_1Solairua_EsperientziaTxokoa1_11.jpeg':
+      document.querySelector('.container_text_maps').innerHTML = `<li class="fs-4">Nombre Laskorain HH3</li>
+                                                                  <li class="fs-4">Ubicación</li>
+                                                                  <li class="fs-4">Fecha</li>
+                                                                  <li class="fs-4">...</li>`; 
+      break;
+    case '../../media/Laskorain_HH4_1Solairua_Zentzumen_Txokoa1_29.jpeg':
+      document.querySelector('.container_text_maps').innerHTML = `<li class="fs-4">Nombre Laskorain HH4</li>
+                                                                  <li class="fs-4">Ubicación</li>
+                                                                  <li class="fs-4">Fecha</li>
+                                                                  <li class="fs-4">...</li>`; 
+      break;
 
-  if (e.keyCode == "37") {
-    showPreviousItem();
-  } else if (e.keyCode == "39") {
-    showNextItem();
+    default:
+      console.log('error');
+      break;
   }
 }
 
-nextItem.addEventListener("click", showNextItem);
-previousItem.addEventListener("click", showPreviousItem);
-document.addEventListener("keydown", keyPress);
+
+/* funcion selector de Vr */
+const itemsVr = document.querySelectorAll('model-viewer.changeVr');
+const vrActive = document.querySelector('#Vr');
+const vrCount = itemsVr.length;
+let countVr = 0;
+
+showNextVr =()=> {
+  if (countVr < vrCount -1) {
+    countVr++;
+  } else {
+    countVr = 0;
+  }
+  let srcVr = itemsVr[countVr];
+  let newSrcVr = srcVr.getAttribute('src');
+  document.querySelector('#Vr').setAttribute('src', newSrcVr);
+
+  switch (newSrcVr) {
+    case '../../media/Barca_01.glb': 
+      document.querySelector('.container_text_maps').innerHTML = `<li class="fs-4">Nombre Barca</li>
+                                                                  <li class="fs-4">Tamaño</li>
+                                                                  <li class="fs-4">Fecha</li>
+                                                                  <li class="fs-4">Material</li>
+                                                                  <li class="fs-4">Utilidad</li>
+                                                                  <li class="fs-4">...</li>`;
+      break;
+    case '../../media/Capitan America (1).glb':
+      document.querySelector('.container_text_maps').innerHTML = `<li class="fs-4">Nombre Capitan América</li>
+                                                                  <li class="fs-4">Tamaño</li>
+                                                                  <li class="fs-4">Fecha</li>
+                                                                  <li class="fs-4">Material</li>
+                                                                  <li class="fs-4">Utilidad</li>
+                                                                  <li class="fs-4">...</li>`; 
+      break;
+    case '../../media/GLB_Arbol_01.glb':
+      document.querySelector('.container_text_maps').innerHTML = `<li class="fs-4">Nombre Arbol</li>
+                                                                  <li class="fs-4">Tamaño</li>
+                                                                  <li class="fs-4">Fecha</li>
+                                                                  <li class="fs-4">Material</li>
+                                                                  <li class="fs-4">Utilidad</li>
+                                                                  <li class="fs-4">...</li>`; 
+      break;
+    case '../../media/GLB_Coche_03.glb':
+      document.querySelector('.container_text_maps').innerHTML = `<li class="fs-4">Nombre Coche</li>
+                                                                  <li class="fs-4">Tamaño</li>
+                                                                  <li class="fs-4">Fecha</li>
+                                                                  <li class="fs-4">Material</li>
+                                                                  <li class="fs-4">Utilidad</li>
+                                                                  <li class="fs-4">...</li>`; 
+      break;
+    case '../../media/GLB_Jaguar.glb':
+      document.querySelector('.container_text_maps').innerHTML = `<li class="fs-4">Nombre Jaguar</li>
+                                                                  <li class="fs-4">Tamaño</li>
+                                                                  <li class="fs-4">Fecha</li>
+                                                                  <li class="fs-4">Material</li>
+                                                                  <li class="fs-4">Utilidad</li>
+                                                                  <li class="fs-4">...</li>`;
+      break;
+
+    default:
+      console.log('error');
+      break;
+  }
+}
+
+showPreviousVr =()=> {
+  if (countVr > 0) {
+    countVr--;
+  } else {
+    countVr = vrCount - 1;
+  }
+  let srcVr = itemsVr[countVr];
+  let newSrcVr = srcVr.getAttribute('src');
+  document.querySelector('#Vr').setAttribute('src', newSrcVr);
+
+  switch (newSrcVr) {
+    case '../../media/Barca_01.glb': 
+      document.querySelector('.container_text_maps').innerHTML = `<li class="fs-4">Nombre Barca</li>
+                                                                  <li class="fs-4">Tamaño</li>
+                                                                  <li class="fs-4">Fecha</li>
+                                                                  <li class="fs-4">Material</li>
+                                                                  <li class="fs-4">Utilidad</li>
+                                                                  <li class="fs-4">...</li>`;
+      break;
+    case '../../media/Capitan America (1).glb':
+      document.querySelector('.container_text_maps').innerHTML = `<li class="fs-4">Nombre Capitan América</li>
+                                                                  <li class="fs-4">Tamaño</li>
+                                                                  <li class="fs-4">Fecha</li>
+                                                                  <li class="fs-4">Material</li>
+                                                                  <li class="fs-4">Utilidad</li>
+                                                                  <li class="fs-4">...</li>`; 
+      break;
+    case '../../media/GLB_Arbol_01.glb':
+      document.querySelector('.container_text_maps').innerHTML = `<li class="fs-4">Nombre Arbol</li>
+                                                                  <li class="fs-4">Tamaño</li>
+                                                                  <li class="fs-4">Fecha</li>
+                                                                  <li class="fs-4">Material</li>
+                                                                  <li class="fs-4">Utilidad</li>
+                                                                  <li class="fs-4">...</li>`; 
+      break;
+    case '../../media/GLB_Coche_03.glb':
+      document.querySelector('.container_text_maps').innerHTML = `<li class="fs-4">Nombre Coche</li>
+                                                                  <li class="fs-4">Tamaño</li>
+                                                                  <li class="fs-4">Fecha</li>
+                                                                  <li class="fs-4">Material</li>
+                                                                  <li class="fs-4">Utilidad</li>
+                                                                  <li class="fs-4">...</li>`; 
+      break;
+    case '../../media/GLB_Jaguar.glb':
+      document.querySelector('.container_text_maps').innerHTML = `<li class="fs-4">Nombre Jaguar</li>
+                                                                  <li class="fs-4">Tamaño</li>
+                                                                  <li class="fs-4">Fecha</li>
+                                                                  <li class="fs-4">Material</li>
+                                                                  <li class="fs-4">Utilidad</li>
+                                                                  <li class="fs-4">...</li>`;
+      break;
+
+    default:
+      console.log('error');
+      break;
+  }
+}
